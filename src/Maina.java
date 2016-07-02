5c1618f2e067c11c8062c83e95bd98b574e3d767
Kate <kateryna.skrem@gmail.com>
2016-07-02 16:31:35 +0300

    Commiting my project  .this is test  number 3

---
+++ src/Maina.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Kate on 02.07.2016.
+ */
+public class Maina {
+
+    public static void main(String[] args) {
+        System.out.println("ergerggergerg");
+        System.out.println ("nnn");
+        System.out.println ("nnn");
+        System.out.println ("nnn");
+        System.out.println ("nnn");
+        System.out.println ("nnn");
+        System.out.println ("nnn");
+        System.out.println ("nnn");
+    }
+}
